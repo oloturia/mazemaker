34da8a0f2dc1bbdcc8f9587ae976ef091d806ac7
oloturia <oloturia@gmail.com>
2020-02-24 19:27:55 +0100

    fixed inversion

--- mumaze.py
+++ mumaze.py
@@ -8,12 +8,12 @@ i2c = machine.I2C(scl=pscl,sda=psda)
 
 oled = ssd1306.SSD1306_I2C(128,32,i2c)
 
-def createMaze(h=18,w=66):
+def createMaze(h=16,w=64):
 	oled.fill(0)
 	maze = mazemaking.mazeMaker(h,w,(2,2),(w-2,h-2))
 	for x,column in enumerate(maze):
 		for y,row in enumerate(column):
-			if row:
+			if row == False:
 				oled.pixel(x*2,y*2,1)
 				oled.pixel(x*2+1,y*2+1,1)
 				oled.pixel(x*2+1,y*2,1)
